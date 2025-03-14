5889fa6e2f658fed54ed2461efa8be77ca2aebf2
ManojNambiraj <postboxmano@gmail.com>
2025-03-14 20:44:42 +0530

    JS Variable & Hoisting & Scoping

---
+++ JS/Class2/script.js
@@ -0,0 +1,66 @@
+// Javascript - > functionalities
+
+    // Built-in functions
+    // User-defined functions
+
+// Javascript Versions:
+
+    // current -> ES6
+    // last    -> ES5
+
+// Variable:
+
+    // ranjith_age_from_Coimbatore = 50;
+
+    // console.log(ranjithAgeFromCoimbatore);
+
+// Hoisting: (Variable declaration)
+
+    // var  --> ES5
+
+        // var a = 10
+
+        // var a = 200
+
+        // console.log(a);
+        
+    // let
+
+        // let a;
+
+        // a = 50
+
+        // console.log(a);
+
+        // a = 200
+
+        // console.log(a);
+
+    // const
+
+        // const a = 20
+
+        // console.log(a);
+
+// Scoping: 
+
+    // Global scope
+
+        {
+            var a = 10
+
+            console.log("inside: ", a);
+        }
+
+        console.log("outside: ", a);
+
+    // Local scope
+
+        // {
+        //   let a = 10;
+
+        //   console.log("inside: ", a);
+        // }
+
+        // console.log("outside: ", a);
+        
