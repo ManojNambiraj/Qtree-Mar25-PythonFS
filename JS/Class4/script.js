411c8f1f627be9e7fd2ba71aa2a58ca6e0d2c563
ManojNambiraj <postboxmano@gmail.com>
2025-03-18 20:50:34 +0530

    JS Control stmts

---
+++ JS/Class4/script.js
@@ -0,0 +1,97 @@
+// Control Statements:
+
+// Conditional stmts:
+
+// if
+
+// let age = 20
+
+// if(age > 18){
+//     console.log("Eligible for voting");
+// }
+
+// if else
+
+// let name = "kavitha"
+
+// if(name === "Raj"){
+//     console.log("Eligible for voting");
+// }else{
+//     console.log("Not eligible");
+// }
+
+// if else if
+
+// let age = 18;
+
+// if (age > 18) {
+
+//     console.log("Eligible for voting");
+
+// } else if(age == 18){
+
+//     console.log("Partially Eligible or Equal")
+
+// } else {
+
+//     console.log("Not Eligible");
+
+// }
+
+// switch case
+
+// let day = "Jan 15"
+
+// switch (day) {
+//   case "Jan 1":
+//     console.log("It's a Monday");
+//     break;
+//   case 2:
+//     console.log("It's a Tuesday");
+//     break;
+//   case 3:
+//     console.log("It's a Wednesday");
+//     break;
+//   case 4:
+//     console.log("It's a Thursday");
+//     break;
+//   case 5:
+//     console.log("It's a Friday");
+//     break;
+//   case 6:
+//     console.log("It's a Saturday");
+//     break;
+//   default:
+//     console.log("Happy Weekend");
+//     break;
+// }
+
+// Looping stmts:
+
+// 1. for loop
+
+// for(stmt1; stmt2; stmt4){
+// ....stmt3....
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// 2. while loop
+
+// let i = 11;
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// 3. do...while loop
+
+let i = 11;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
