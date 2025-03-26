6b6ddcc21ab878f2d41957af351f2b466747ebff
ManojNambiraj <postboxmano@gmail.com>
2025-03-26 20:43:12 +0530

    python 1st class

---
+++ python/class1/first.py
@@ -0,0 +1,30 @@
+# print("hi")
+
+# Variable
+
+    # a = "hello, Good evening"
+
+    # print(a)
+
+    # a, b = "apple", "banana"
+
+    # print(a)
+    # print(b)
+
+    # a = b = c = "apple"
+
+    # print(a)
+    # print(b)
+    # print(c)
+
+# Datatypes:
+
+# a = 10
+# a = 10.5
+# a = "Hello"
+# a = 1j
+# a = True
+a = None
+
+print(a, type(a))
+
