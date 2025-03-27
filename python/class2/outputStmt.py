0104216064cefd8da97cad982ca7e3656686a2cd
ManojNambiraj <postboxmano@gmail.com>
2025-03-27 20:43:01 +0530

    python strings

---
+++ python/class2/outputStmt.py
@@ -0,0 +1,59 @@
+# Casting
+
+    # """
+    #     a = 9
+
+    #     b = int (a)
+    #     b = float (a)
+
+    #     print(b, type(b))
+    # """
+
+# Strings:
+
+    # txt = """Hello, 
+    # good evening, 
+    # How are you, 
+    # How about your day?"""
+
+    # print(txt)
+
+    # txt = "Hello, wor l d...!"
+    # #      01234567891011............
+
+    # print(txt[6])
+
+# Slicing -> forward indexing
+
+    # txt = "Hello, world"
+    #               -2-1
+
+    # print(txt[7:])
+
+# Slicing -> backward indexing
+
+    # print(txt[-5:])
+
+# Modify String
+
+    # txt = "Hello.world"
+
+    # print(txt.upper())
+    # print(txt.lower())
+    # print(txt.replace("H", "J"))
+    # print(txt.split("."))
+
+# String concate
+
+    # txt1 = "hello"
+    # txt2 = "world"
+
+    # print(txt1 + " " + txt2)
+
+# Format strings:
+
+age = 25
+
+txt = f"Hello, I am Jeeva, My age is: {age}"
+
+print(txt)
