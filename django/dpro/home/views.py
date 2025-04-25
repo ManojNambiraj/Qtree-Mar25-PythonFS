e24356ff8b93571908967d37efc68ed3fd7a59c8
ManojNambiraj <postboxmano@gmail.com>
2025-04-25 20:52:13 +0530

    Django passing value to pages & Logging

--- django/dpro/home/views.py
+++ django/dpro/home/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+import logging
 
-# Create your views here.
+datas = [
+    {"id": 1, "title": "Post1",  "content": "Some quick example text for the post's content1.", "area":"Sportz", "size":20},
+    {"id": 2, "title": "Post2",  "content": "Some quick example text for the post's content2.", "area":"Development"},
+    {"id": 3, "title": "Post3",  "content": "Some quick example text for the post's content3.", "area":"Engineering"},
+    {"id": 4, "title": "Post4",  "content": "Some quick example text for the post's content4.", "area":"Kids"},
+]
+
 def index(request):
 
     title = "Home Page"
 
-    datas = [
-        {"id": 1, "title": "Post1",  "content": "Some quick example text for the post's content1.", "area":"Sportz"},
-        {"id": 2, "title": "Post2",  "content": "Some quick example text for the post's content2.", "area":"Development"},
-        {"id": 3, "title": "Post3",  "content": "Some quick example text for the post's content3.", "area":"Engineering"},
-        {"id": 4, "title": "Post4",  "content": "Some quick example text for the post's content4.", "area":"Kids"},
-    ]
-
     return render(request, "index.html", {'title': title, "datas": datas})
 
 def details(request, id):
-    return render(request, "detail.html", {"id": id})
+    
+    post = next((item for item in datas if item["id"] == id), None)
+
+    # logger = logging.getLogger("Testing")
+    # logger.debug(post)
+
+    return render(request, "detail.html", {"post": post})
